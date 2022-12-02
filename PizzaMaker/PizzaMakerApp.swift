5564b7e5f861ad5db36c270a80e3e92e00e40d5c
Alistiar White <awhite@bsd220.org>
2022-12-02 09:48:22 -0600

    Initial Commit

---
+++ PizzaMaker/PizzaMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaMakerApp.swift
+//  PizzaMaker
+//
+//  Created by Alistair White on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
