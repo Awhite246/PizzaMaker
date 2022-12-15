88e758c1a87550ccdb27ca705253a99eb84863d1
Alistiar White <awhite@bsd220.org>
2022-12-15 09:06:38 -0600

    Observable object to store favorited pizzas

---
+++ PizzaMaker/Helpers/FavoriteList.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteList.swift
+//  PizzaMaker
+//
+//  Created by Alistair White on 12/15/22.
+//
+
+import SwiftUI
+
+class FavoriteList : ObservableObject {
+    @Published var items = [Pizza]()
+    
+    func add(item : Pizza) {
+        if (items.firstIndex(of: item) == nil) {
+            items.append(item)
+        }
+    }
+    
+    func remove(item : Pizza) {
+        if let index = items.firstIndex(of: item) {
+            items.remove(at: index)
+        }
+    }
+}
