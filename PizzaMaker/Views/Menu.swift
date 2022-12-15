235391bd9fc04f34b714c6b4b563f2749b22d8b1
Alistiar White <awhite@bsd220.org>
2022-12-14 20:14:45 -0600

    Added menu

--- PizzaMaker/Views/Menu.swift
+++ PizzaMaker/Views/Menu.swift
@@ -10,22 +10,67 @@ import SwiftUI
 struct Menu: View {
     let menu = Bundle.main.decode([Pizza].self, from: "menu.json")
     @State var showingDetail = false
+    @State var allItems = true
     var body: some View {
         VStack {
             ZStack {
+                VStack {
+                    Color("Imperial Red")
+                        .frame(height: 150)
+                        .padding(.bottom, -10)
+                    HStack {
+                        Color("Celadon Blue")
+                            .frame(height: 50)
+                    }
+                }
                 Text("Menu")
                     .font(.title).bold()
-                    .foregroundColor(Color("Imperial Red"))
+                    .foregroundColor(Color("Honeydew"))
+                HStack {
+                    Button {
+                        withAnimation {
+                            allItems = true
+                        }
+                    } label: {
+                        VStack {
+                            Image(systemName: "square.grid.2x2.fill")
+                            Text("All Items")
+                        }
+                    }
+                    .foregroundColor(Color(!allItems ? "Honeydew" : "Pastel Pink"))
+                    .padding(.trailing, 50)
+                    Button {
+                        withAnimation {
+                            allItems = false
+                        }
+                    } label: {
+                        VStack {
+                            Image(systemName: "tag.circle")
+                            Text("All Specials")
+                        }
+                    }
+                    .foregroundColor(Color(allItems ? "Honeydew" : "Pastel Pink"))
+                    .padding(.leading, 50)
+                    .offset(x: 10)
+                }
+                .offset(y: 75)
             }
+            .ignoresSafeArea()
             ScrollView {
                 LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
                     ForEach(menu) { pizza in
-                        HomeItem(pizza: pizza)
+                        if allItems {
+                            MenuItem(pizza: pizza)
+                        } else {
+                            if pizza.dailyDeal || pizza.trending || pizza.favorite {
+                                MenuItem(pizza: pizza)
+                            }
+                        }
                     }
                 }
             }
+            .padding(.top, -65)
         }
-        .background(LinearGradient(gradient: Gradient(colors: [Color("Powder Blue"), Color("Pastel Pink")]), startPoint: .top, endPoint: .bottom))
     }
 }
 struct Menu_Previews: PreviewProvider {
--- PizzaMaker/Views/MenuItem.swift
+++ PizzaMaker/Views/MenuItem.swift
@@ -1,31 +1,28 @@
 //
-//  HomeItem.swift
+//  MenuItem.swift
 //  PizzaMaker
 //
-//  Created by Alistair White on 12/13/22.
+//  Created by Alistair White on 12/14/22.
 //
 
 import SwiftUI
 
-struct HomeItem: View {
+struct MenuItem: View {
     let pizza: Pizza
     @State private var like = false
-    @State private var size = 20.0
+    @State private var size = 15.0
     @State private var showingDetail = false
     var body: some View {
         VStack {
             ZStack {
                 Color("Honeydew")
-                    .frame(width: 200, height: 300)
+                    .frame(width: 180, height: 290)
                     .overlay {
                         Image(pizza.name)
                             .resizable()
-                            .frame(width: 200, height: 200)
+                            .frame(width: 180, height: 180)
                             .ignoresSafeArea()
-                            .offset(y: -50)
-                        LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.9), Color.clear]), startPoint: .top, endPoint: .bottom)
-                            .offset(y: -130)
-                            .frame(width: 200, height: 40)
+                            .offset(y: -55)
                     }
                     .clipShape(RoundedRectangle(cornerRadius: 15))
                 HStack {
@@ -34,28 +31,32 @@ struct HomeItem: View {
                             RadialGradient(gradient: Gradient(colors: [Color.black, Color.clear]), center: .center, startRadius: 5, endRadius: 11)
                                 .frame(width: 25, height: 25)
                             Image(systemName: "star.circle.fill")
+                                .resizable()
+                                .frame(width: 17, height: 17)
                                 .foregroundColor(Color("Tangerine"))
                         }
-                        .offset(x: pizza.trending ? 0 : -17)
+                        .offset(x: pizza.trending ? 0 : -14)
                     }
                     if pizza.trending {
                         ZStack {
                             RadialGradient(gradient: Gradient(colors: [Color.black, Color.clear]), center: .center, startRadius: 5, endRadius: 11)
                                 .frame(width: 25, height: 25)
                             Image(systemName: "flame.circle.fill")
+                                .resizable()
+                                .frame(width: 17, height: 17)
                                 .foregroundColor(Color("Vermilion"))
                         }
-                        .offset(x: pizza.dailyDeal ? 0 : -17)
+                        .offset(x: pizza.dailyDeal ? -2 : -18)
                     }
                 }
-                .offset(x: -60, y: -125)
+                .offset(x: -50, y: -120)
                 Button {
                     like.toggle()
                     withAnimation(){
                         if like {
-                            size += 5
+                            size += 3
                         } else {
-                            size -= 5
+                            size -= 3
                         }
                     }
                 } label: {
@@ -65,16 +66,12 @@ struct HomeItem: View {
                         .frame(width: size, height: size)
                 }
                 .animation(.interpolatingSpring(stiffness: 100, damping: 7), value: size)
-                .offset(x: -75, y: 70)
-                Text(like ? "You like this pizza!" : "Like this pizza!")
-                    .foregroundColor(Color("Pastel Pink"))
-                    .multilineTextAlignment(.leading)
-                    .offset(x: like ? 15 : 5, y: 70)
+                .offset(x: 75, y: 50)
                 Text(pizza.name)
                     .bold()
                     .font(.system(size: 20))
                     .foregroundColor(Color("Prussian Blue"))
-                    .offset(y: 90)
+                    .offset(y: 65)
                 HStack {
                     Text("$\(String(format: "%.2f", (pizza.dailyDeal ? (pizza.sPrice * 0.8) : (pizza.sPrice))))")
                         .foregroundColor(Color("Prussian Blue"))
@@ -84,7 +81,7 @@ struct HomeItem: View {
                             .foregroundColor(Color(uiColor: .lightGray))
                     }
                 }
-                .offset(y: 110)
+                .offset(y: 90)
                 .font(.system(size: 15))
                 Button {
                     showingDetail = true
@@ -92,7 +89,7 @@ struct HomeItem: View {
                     Text("View Details")
                         .foregroundColor(Color("Celadon Blue"))
                 }
-                .offset(y: 130)
+                .offset(y: 115)
             }
         }
         .fullScreenCover(isPresented: $showingDetail) {
@@ -101,14 +98,14 @@ struct HomeItem: View {
         .onAppear {
             if pizza.favorite {
                 like = true
-                size = 20.0
+                size = 18.0
             }
         }
     }
 }
 
-struct HomeItem_Previews: PreviewProvider {
+struct MenuItem_Previews: PreviewProvider {
     static var previews: some View {
-        HomeItem(pizza: Pizza(id: UUID(uuidString: "5d5198e0-5389-4e62-bb1f-55680b977af5")!, name: "Cheese", sPrice: 1.00, mPrice: 2.00, lPrice: 3.00, ingredients: [Ingredient(id: UUID(uuidString: "1142be9c-fffb-4b78-87cf-5259cc85036c")!, name: "Test", count: 1)], description: "This cheese pizza is the perfect classic. It's made with a hand-stretched thin crust, topped with a delicious tomato sauce, and covered with a blanket of melty mozzarella cheese. To finish it off, it's baked in our hot brick oven, giving it a crispy, golden-brown crust. Get ready to enjoy the perfect combination of tangy tomato sauce, gooey cheese, and a light, flaky crust.", trending: true, dailyDeal: true, favorite: true))
+        MenuItem(pizza: Pizza(id: UUID(uuidString: "5d5198e0-5389-4e62-bb1f-55680b977af5")!, name: "Cheese", sPrice: 1.00, mPrice: 2.00, lPrice: 3.00, ingredients: [Ingredient(id: UUID(uuidString: "1142be9c-fffb-4b78-87cf-5259cc85036c")!, name: "Test", count: 1)], description: "This cheese pizza is the perfect classic. It's made with a hand-stretched thin crust, topped with a delicious tomato sauce, and covered with a blanket of melty mozzarella cheese. To finish it off, it's baked in our hot brick oven, giving it a crispy, golden-brown crust. Get ready to enjoy the perfect combination of tangy tomato sauce, gooey cheese, and a light, flaky crust.", trending: true, dailyDeal: false, favorite: true))
     }
 }
